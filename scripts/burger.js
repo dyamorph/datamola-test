eebbea8bbc3d79b31d4dc36362fe7682a26a19c6
dyamorph <dyamorph22@gmail.com>
2023-01-16 19:38:14 +0300

    fix z-index and scroll lock

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,14 +1,18 @@
 const burger = document.querySelector('.menu_burger-icon')
 const navMenu = document.querySelector('.menu_list')
+const body = document.querySelector('body')
+console.log(body)
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   navMenu.classList.toggle('active')
+  body.classList.toggle('lock')
 })
 
 document.querySelectorAll('.menu_link').forEach(link=> {
   link.addEventListener('click', () => {
     burger.classList.remove('active')
     navMenu.classList.remove('active')
+    body.classList.remove('lock')
   })
 })
